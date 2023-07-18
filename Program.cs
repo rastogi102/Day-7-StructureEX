241d1edf3158b117cb57c72f7b035a67ed56d9c0
rastogi102 <nikhilrastogi102@gmail.com>
2023-07-18 14:14:59 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace StructExample
+{
+    public struct Rectangle
+    {
+        public int width;
+        public int height;
+
+      
+        public void Area()
+        {
+            int area = height * width;
+            Console.WriteLine($"Area of Rectangle {height}*{width} ={area}");
+
+        }
+    }
+    public class Myclass
+    {
+        public static void Main()
+        {
+            Rectangle rect;
+            Console.WriteLine("Enter Height");
+            rect.height=int.Parse( Console.ReadLine() );
+            Console.WriteLine("Enter Width");
+            rect.width=int.Parse( Console.ReadLine() );
+            rect.Area();
+            Console.ReadKey();
+        }
+    }
+ }
